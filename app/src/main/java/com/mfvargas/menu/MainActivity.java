849df07e7b754b42df6dc134a86c07a71775cbfa
Mafe0394 <mfvargasm0394@gmail.com>
2019-10-29 14:33:31 -0500

    Creación y manejo de menú de contexto

--- app/src/main/java/com/mfvargas/menu/MainActivity.java
+++ app/src/main/java/com/mfvargas/menu/MainActivity.java
@@ -5,8 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.style.LocaleSpan;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +20,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Declaramos TextView
+        TextView tvNombre=findViewById(R.id.nombrePersona);
+        //Disponible para levantar view de contexto
+        registerForContextMenu(tvNombre);
     }
 
+    //Menu de opciones
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //Se sobreescribe este método para inflar el menu de opciones
@@ -40,5 +52,28 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //Menu de contexto
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        //Inflamos el menú de contexto
+        MenuInflater inflater=new MenuInflater(this);
+        getMenuInflater().inflate(R.menu.menu_contexto,menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_edit:
+                Toast.makeText(this,getResources().getString(R.string.menu_edit),Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_delete:
+                Toast.makeText(this,getResources().getString(R.string.menu_delete),Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onContextItemSelected(item);
     }
 }
